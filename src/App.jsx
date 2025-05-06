364b8bb00e95d6756d635ecc70be3cce42ff86cf
Nisha-Kedia <b321054@iiit-bh.ac.in>
2025-05-06 23:00:55 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,32 @@ import "./App.css"
 import ai from "./assets/ai.png"
 import { CiMicrophoneOn } from "react-icons/ci"
 import { datacontext } from './context/UserContext'
+import speak from './assets/speak.gif'
+import aigif from './assets/aiVoice.gif'
 
 function App() {
- // let {recognition,speaking,setSpeaking}=useContext(datacontext)
+  let {recognition,speaking,setSpeaking,prompt,setPrompt,response}=useContext(datacontext)
 
   return (
     <div className='main'>
       <img src={ai} alt="" id="sifra"/>
       <span>I'm Sifra , your advanced virtual assistant</span>
-      
+      {!speaking?
       <button onClick={()=>{
+        setPrompt("Listening...")
         setSpeaking(true)
         recognition.start()
       }}>Click Here <CiMicrophoneOn/> </button>
       
+      :
+      <div className='response'>
+        {!response? <img src={speak} alt="" id="speak"/> :
+        <img src={aigif} alt="" id="ai"/>
+        }
+        <p>{prompt}</p>
+      </div>
+    
+    }
     </div>
   )
 }
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,34 +1,46 @@
-import React, {createContext} from 'react'
+import React, {createContext , useState} from 'react'
 import run from '../gemini'
 export const datacontext=createContext()
 
 function UserContext({children}) {
-  //let [speaking,setSpeaking]=useState(false)
+  let [speaking,setSpeaking]=useState(false)
+  let [prompt,setPrompt]=useState("listening...")
+  let [response,setResponse]=useState(false)
 
     function speak(text)
     {
         let text_speak=new SpeechSynthesisUtterance(text)
         text_speak.volume=1;
-        text_speak.rate=1;
+        text_speak.rate=2;
         text_speak.pitch=1;
         text_speak.lang="hi-GB"
         window.speechSynthesis.speak(text_speak)
     }
     async function aiResponse(prompt){
       let text =await run(prompt)
+      setPrompt(text)
       speak(text)
+      setResponse(true)
+      setTimeout(()=>{
+      setSpeaking(false)
+      },5000)
     }
     let speechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition
     let recognition=new speechRecognition()
     recognition.onresult=(e)=>{
       let currentIndex=e.resultIndex
       let transcript=e.results[currentIndex][0].transcript
-      console.log(transcript);
+      setPrompt(transcript)
       aiResponse(transcript)
     }
 
     let value={
       recognition,
+      speaking,
+      setSpeaking,
+      prompt,
+      setPrompt,
+      response
     }
   return (
     <div>
